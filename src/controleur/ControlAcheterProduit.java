f448eea4e021c07978f9a1becfad043f6b1c5917
Quentin Kouzmitch <quentinvegeto@gmail.com>
2023-03-10 15:34:07 +0100

    DernierExo

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -27,4 +27,8 @@ public class ControlAcheterProduit {
 	public String[] infosMarche() {
 		return village.donnerEtatMarche();
 	}
+	
+	public int acheterProduit(int quantite) {
+		//cherche acheter produit dans etal
+	}
 }
