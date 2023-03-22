89457612026e3082dae08037df8cc2b0f7e41104
Quentin Kouzmitch <quentinvegeto@gmail.com>
2023-03-22 20:28:52 +0100

    fini

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Etal;
 import villagegaulois.Village;
 
@@ -19,16 +20,45 @@ public class ControlAcheterProduit {
 	public boolean habitantVillage(String nomAcheteur) {
 		return controlVerifierIdentite.verifierIdentite(nomAcheteur);
 	}
+	private Gaulois[] gauloisVendeurs(String produit) {
+		Gaulois[] gaulois = village.rechercherVendeursProduit(produit);
+		return gaulois;
+	}
+	
+	public String[] vendeursProduit(String produit) {
+		Gaulois[] gauloisVendeurs = gauloisVendeurs(produit);
+		if (gauloisVendeurs == null) {
+			return  new String[0];
+		}
+		String[] vendeurs = new String[gauloisVendeurs.length];
+		for (int i = 0; i<gauloisVendeurs.length;i++) {
+			vendeurs[i] = gauloisVendeurs[i].getNom();
+		}
+		return vendeurs;
+	}
 	
 	public Etal etalVendeur(String nomVendeur) {
 		return controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
 	}
 	
-	public String[] infosMarche() {
-		return village.donnerEtatMarche();
+	private Etal trouverVendeur(Gaulois gaulois) {
+		return village.rechercherEtal(gaulois);
 	}
 	
-	public int acheterProduit(int quantite) {
-		//cherche acheter produit dans etal
+	
+	public int acheterProduit(int quantite,int commercantChoisis, String Produit, String Acheteur) {
+		Gaulois[] vendeurs = gauloisVendeurs(Produit);
+		Gaulois vendeurChoisis = vendeurs[commercantChoisis];
+		Etal etal = trouverVendeur(vendeurChoisis);
+		int quantiteEtal = etal.getQuantite();
+		if (quantiteEtal == 0) {
+			System.out.println(Acheteur + " veut acheter " + quantite + Produit + ", malheureusement il n'y en a plus !");
+		}else if (quantiteEtal < quantite){
+			System.out.println(Acheteur + " veut acheter " + quantite + Produit + ", malheureusement " + vendeurChoisis.getNom() + " n'en a plus que "
+					+ quantiteEtal + ". " + Acheteur + " achète tout le stock de " + vendeurChoisis.getNom() + ".");
+		}else {
+			System.out.println(Acheteur + " achète " + quantite + Produit + " à " + vendeurChoisis.getNom() + ".");
+		}
+		return etal.acheterProduit(quantite);
 	}
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -18,26 +18,21 @@ public class BoundaryAcheterProduit {
 		}else {
 			System.out.println("Quel produit voulez-vous acheter ?");
 			String produit = scan.nextLine();
-			String[] infosMarche = controlAcheterProduit.infosMarche();
-			String[] vendeurProduit = null;
-			int j = 0;
-			for (int i = 2; i<infosMarche.length;i+=1) {
-				if (infosMarche[i] == produit) {
-					vendeurProduit[j] = infosMarche[i-1];j++;
-				}
-			}
-			if (vendeurProduit.length == 0) {
+			String[] vendeursProduit = controlAcheterProduit.vendeursProduit(produit);
+			if (vendeursProduit.length == 0) {
 				System.out.println("Aucun vendeur ne vend ce produit.");
 			}else {
 				System.out.println("Chez quel commerçant voulez-vous acheter des " + produit + "?");
-				for (int i = 0; i<vendeurProduit.length;i++) {
-					System.out.println(i + " - " + vendeurProduit[i]);
+				for (int i = 0; i<vendeursProduit.length;i++) {
+					System.out.println(i + " - " + vendeursProduit[i]);
 				}
-				int commerçantChoisis = scan.nextInt();
-				System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " + vendeurProduit[commerçantChoisis]);
+				int commercantChoisis = scan.nextInt();
+				System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " + vendeursProduit[commercantChoisis]);
 				System.out.println("Bonjour " + nomAcheteur);
 				System.out.println("Combien de " + produit + "voulez-vous acheter ?");
-				int valeurAchete = scan.nextInt();
+				int valeurAcheter = scan.nextInt();
+				controlAcheterProduit.acheterProduit(valeurAcheter, commercantChoisis, produit, nomAcheteur);
+				
 			}
 		}
 	}
